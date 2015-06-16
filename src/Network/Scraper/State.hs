eb71362e29def600b11b07bc70a0cee869f30dc2
Cody Goodman <codygman.consulting@gmail.com>
2015-06-16 01:53:31 -0500

    update runScraper functions types

--- src/Network/Scraper/State.hs
+++ src/Network/Scraper/State.hs
@@ -83,10 +83,14 @@ withInitialDbgState callback = withSession $ \s -> do
                         }
   callback initialState
 
-runScraper :: Scraper a -> IO (Either e a)
+-- runScraper :: Scraper a -> IO (Either e a)
+-- TODO: Why doesn't above type signature work?
+runScraper :: ExceptT e (ST.StateT ScraperState IO) a -> IO (Either e a)
 runScraper k = withInitialState (evalScraperWith k)
 
-runScraperDebug :: Scraper a -> IO (Either e a)
+-- runScraperDebug :: Scraper a -> IO (Either e a)
+-- TODO: Why doesn't above type signature work?
+runScraperDebug :: ExceptT e (ST.StateT ScraperState IO) a -> IO (Either e a)
 runScraperDebug k = withInitialDbgState (evalScraperWith k)
 
 evalScraperWith
